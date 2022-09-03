182dc7df2c8abf34c2f38e3073d85aa6fba69885
Manuella Dias Cruz <manuella.dias@datarisk.io>
2022-09-02 22:26:22 -0300

    chore: trying to fix bug

--- src/pages/Countdown/index.js
+++ src/pages/Countdown/index.js
@@ -16,29 +16,40 @@ export default function Countdown() {
 
   //http://localhost:3000/?code=43421690725e06f5100d
 
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log('urlParams, href = ', window.location.href);
+  console.log('urlParams, search = ', window.location.search);
+  const code = urlParams.get('code');
+  console.log('urlParams, code = ', code);
+  
+  if(code) {
+    const promise = axios.post(`${process.env.REACT_APP_API_BASE_URL}/oauth`, { code });
+    promise.then((response) => {
+      console.log(response);
+      alert('você está logado, meu chapa! dá uma olhada no console!');
+      navigate('/dashboard');
+    });
+    promise.catch((error) => {
+      console.log(error);
+      alert('ops, deu algum xabú');
+    });      
+    // const user = response.data;
+    // console.log(user);
+  }
+
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    console.log('urlParams, href = ', window.location.href);
-    console.log('urlParams, search = ', window.location.search);
-    const code = urlParams.get('code');
-    console.log('urlParams, code = ', code);
-    
-    if(code) {
-      try {
-        navigate('/dashboard');
-        console.log(`${process.env.REACT_APP_API_BASE_URL}/auth`);
-        const response = axios.post(`${process.env.REACT_APP_API_BASE_URL}/sign-in`, { code });
-        const user = response.data;
-        //alert('você está logado, meu chapa! dá uma olhada no console!');
-        console.log(user);
-      } catch (err) {
-        alert('ops, deu algum xabú');
-        console.log('err', err);
-      }
-    } else if (countdownOver) {
+    if (countdownOver) {
       navigate('/enroll');
     }
   }, [countdownOver]);
+
+  function onZero() {
+    navigate('/enroll');
+  }
+
+  if (loadingEventInfo) {
+    return 'Loading...';
+  }  
 
   function onZero() {
     navigate('/enroll');
